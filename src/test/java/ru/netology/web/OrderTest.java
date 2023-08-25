90694e6f6117eaffa49f6e12eea97bc9f3f6d2ed
Sheinart <sheinart09@gmail.com>
2023-08-25 15:00:07 +0300

    initial comit

--- src/test/java/ru/netology/web/OrderTest.java
+++ src/test/java/ru/netology/web/OrderTest.java
@@ -1,5 +1,4 @@
 package ru.netology.web;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -7,19 +6,15 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
-class CallbackTest {
+public class OrderTest {
     private WebDriver driver;
 
     @BeforeAll
-    static void setUpAll() {
+    static void setDriver() {
         WebDriverManager.chromedriver().setup();
     }
 
@@ -30,15 +25,23 @@ class CallbackTest {
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
-        driver.get("http://localhost:9999");
+        driver.get("http://localhost:9999/");
     }
 
     @AfterEach
-    void tearDown() {
+    void setDown() {
         driver.quit();
         driver = null;
     }
 
-
+    @Test
+    void shouldReturnSuccessPage() {
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Остафьева Анна");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79046441684");
+        driver.findElement(By.cssSelector(".checkbox__box")).click();
+        driver.findElement(By.cssSelector(".button__text")).click();
+        String actual = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText().trim();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        assertEquals(expected, actual);
+    }
 }
-
